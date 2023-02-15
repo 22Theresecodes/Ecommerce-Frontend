813df5428713b20990d28863fc1a71bb51c67c55
22Theresecodes <105355739+22Theresecodes@users.noreply.github.com>
2023-02-15 23:03:50 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./components/Navbar";
+import Announcement from "./components/Announcement";
+import Slider  from "./components/Slider";
+
+function App() {
+  return (
+    <>
+    <Announcement/>
+   <Navbar/>
+   <Slider/>
+   </>
+  )
+
+  }
+export default App;
